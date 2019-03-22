34fadc309c40b44fb6375b929fb91019e66bac65
Kelly Dwan <ryelle@users.noreply.github.com>
2019-03-22 19:40:15 -0400

    Enqueue CSS with a dynamic version based on modified time

--- functions.php
+++ functions.php
@@ -7,13 +7,16 @@
  * @package wcus-2019
  */
 
-add_action( 'wp_enqueue_scripts', 'campsite_2017_parent_theme_enqueue_styles' );
-
 /**
  * Enqueue child theme style and dequeue the parent style.
  */
-function campsite_2017_parent_theme_enqueue_styles() {
-	// Dequeue the parent style because we'll replace the whole stylesheet via Remote CSS.
+function wcus_2019_enqueue_styles() {
+	$version = '1.0.0';
+	if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) {
+		$version = filemtime( dirname( __FILE__ ) . '/style.css' );
+	}
+
 	wp_dequeue_style( 'campsite-2017-style' );
-	wp_enqueue_style( 'wcus-2019-style', get_stylesheet_directory_uri() . '/style.css', array(), '1.0.0' );
+	wp_enqueue_style( 'wcus-2019-style', get_stylesheet_directory_uri() . '/style.css', array(), $version );
 }
+add_action( 'wp_enqueue_scripts', 'wcus_2019_enqueue_styles', 12 );
